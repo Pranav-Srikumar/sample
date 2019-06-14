ec31b105e834d92f96ca5b7df83a5dc33358afa1
Pranav Srikumar <51776036+Pranav-Srikumar@users.noreply.github.com>
2019-06-14 18:50:06 +0530

    Create romantonum.py

---
+++ romantonum.py
@@ -0,0 +1,5 @@
+a =(raw_input())
+l1=["Zero","I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII","XIV","XV","XVI","XVII","XVIII","XIX","XX"]
+for i in range(21):
+  if a==l1[i]:
+    print i
