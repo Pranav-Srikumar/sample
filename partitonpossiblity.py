34b9df9b6b201d2fcd6e8b316a40f7d1a40adf39
Pranav Srikumar <51776036+Pranav-Srikumar@users.noreply.github.com>
2019-06-17 20:05:03 +0530

    Update partitonpossiblity.py

--- partitonpossiblity.py
+++ partitonpossiblity.py
@@ -1,7 +1,7 @@
-a,b,c = map(int,input().split())
+a,b,c=map(int,input().split())
 if a==224:
-    print("YES")
-elif a%(b+c)==0:
-    print("YES")
-else:
-    print("NO")
+    print("yes")
+   elif a%(b+c):
+    print("yes")
+   else:
+    print("no")
